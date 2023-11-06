104b9ee0b36003a745a6eec73fd2c6d59bcc917c
Quinten De Man <quinten.deman@gmail.com>
2023-11-06 09:22:40 -0500

    added function to test if its semisorted

--- test/semisort_tests.cpp
+++ test/semisort_tests.cpp
@@ -1,19 +1,32 @@
 #include <gtest/gtest.h>
+#include <random>
 #include "../include/semisort.h"
 
 
 bool semisorted(parlay::sequence<int> records) {
+    int last = records[0];
+    std::set<int> records_seen;
+    for (int i = 0; i < records.size(); i++) {
+        if (records[i] != last) {
+            if (records_seen.find(records[i]) != records_seen.end())
+                return false;
+            last = records[i];
+            records_seen.insert(records[i]);
+        }
+    }
     return true;
 }
 
 TEST(EulerTourTreeSuite, stress_test) {
     // Test parameters
     int input_size = 100;
+    int min_value = 0;
+    int max_value = 10;
 
     // Generate a random sequence of ints and call our semisort function
     parlay::sequence<int> input(input_size);
     for (int i = 0; i < input.size(); i++)
-        input[i] = rand();
+        input[i] = min_value+(rand()%(max_value-min_value));
     parlay::sequence<int> output = semisort(input);
 
     // Check that the results are in semisorted order
